bdea7d07e36985397c35ba17bbc51e1e28c2e5fd
xdaluca <xdaluca@pm.me>
2023-06-08 12:03:02 +0100

    chain support

---
+++ src/sdk/chains.ts
@@ -0,0 +1,50 @@
+export const chains = [
+    {
+      id: 1,
+      name: "Ethereum",
+      color: "#627EEA",
+      icon: "/img/ethereum.png",
+    },
+    {
+      id: 10,
+      name: "Optimism",
+      color: "#FF0420",
+      icon: "/img/optimism.png",
+    },
+    {
+      id: 56,
+      name: "Binance Smart Chain",
+      color: "#F0B90B",
+      icon: "/img/binance.png",
+    },
+    {
+      id: 100,
+      name: "Gnosis Chain",
+      color: "#0d8e74",
+      icon: "/img/gnosis.png",
+    },
+    {
+      id: 137,
+      name: "Polygon",
+      color: "#8B36D7",
+      icon: "/img/polygon.png",
+    },
+    {
+      id: 250,
+      name: "Fantom",
+      color: "#1969ff",
+      icon: "/img/fantom.png",
+    },
+    {
+      id: 42161,
+      name: "Arbitrum",
+      color: "#14A9FD",
+      icon: "/img/arbitrum.png",
+    },
+    {
+      id: 43114,
+      name: "Avalanche",
+      color: "#e84142",
+      icon: "/img/avalanche.png",
+    },
+  ];
